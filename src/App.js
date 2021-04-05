060f949838393cbbd1f5839c2c5188d7947ea6e2
AdalCardona <acardona3718@ppsc.edu.mx>
2021-04-05 18:21:18 -0500

    Generador de memes

--- src/App.js
+++ src/App.js
@@ -1,25 +1,62 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import React from "react";
+class App extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      top: "",
+      bottom: "",
+    };
+  }
 
-function App() {
+  handleChange = (event) => {
+    const { name, value } = event.target;
+    this.setState({ [name]: value });
+  };
+  render() {
+    return (
+      <div className="App">
+        <div className="container">
+          <h1 className="title">Genera tu meme</h1>
+          <Meme top={this.state.top} bottom={this.state.bottom} />
+          <MemeForm values={this.state} onChange={this.handleChange} />
+        </div>
+      </div>
+    );
+  }
+}
+function Meme(props) {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="meme-template">
+      <img
+        src="https://i.pinimg.com/474x/de/47/82/de47820ea7961d454961102c3a4e91eb.jpg"
+        alt="plantilla"
+        className="image-template"
+      />
+      <h2 className="top-text">{props.top}</h2>
+      <h2 className="bottom-text">{props.bottom}</h2>
     </div>
   );
 }
 
+function MemeForm(props) {
+  return (
+    <form>
+      <input
+        className="form-input"
+        name="top"
+        value={props.values.top}
+        placeholder="Texto superior"
+        onChange={props.onChange}
+      />
+      <input
+        className="form-input"
+        name="bottom"
+        value={props.values.bottom}
+        placeholder="Texto inferior"
+        onChange={props.onChange}
+      />
+    </form>
+  );
+}
 export default App;
