5d04ece2cf72c991d71349e892bb52e3c71555b8
AdalCardona <acardona3718@ppsc.edu.mx>
2021-04-07 18:13:25 -0500

    Mejoramiento

--- src/App.js
+++ src/App.js
@@ -1,14 +1,25 @@
 import "./App.css";
 import React from "react";
+
 class App extends React.Component {
   constructor() {
     super();
     this.state = {
       top: "",
       bottom: "",
+      memes: [],
+      template:
+        "https://i.pinimg.com/474x/de/47/82/de47820ea7961d454961102c3a4e91eb.jpg",
     };
   }
+  componentDidMount() {
+    fetch("https://api.imgflip.com/get_memes")
+      .then((response) => response.json())
 
+      .then((memesJson) => {
+        this.setState({ memes: memesJson.data.memes });
+      });
+  }
   handleChange = (event) => {
     const { name, value } = event.target;
     this.setState({ [name]: value });
@@ -18,21 +29,36 @@ class App extends React.Component {
       <div className="App">
         <div className="container">
           <h1 className="title">Genera tu meme</h1>
-          <Meme top={this.state.top} bottom={this.state.bottom} />
+          <Meme
+            top={this.state.top}
+            bottom={this.state.bottom}
+            template={this.state.template}
+          />
           <MemeForm values={this.state} onChange={this.handleChange} />
         </div>
+        <div className="images">
+          {this.state.memes.map((item) => {
+            return (
+              <img
+                src={item.url}
+                className="imageMeme"
+                onClick={() => {
+                  this.setState({ template: item.url });
+                }}
+              />
+            );
+          })}
+        </div>
+        <p>Github:AdalCardona</p>
       </div>
     );
   }
 }
+
 function Meme(props) {
   return (
     <div className="meme-template">
-      <img
-        src="https://i.pinimg.com/474x/de/47/82/de47820ea7961d454961102c3a4e91eb.jpg"
-        alt="plantilla"
-        className="image-template"
-      />
+      <img src={props.template} alt="plantilla" className="image-template" />
       <h2 className="top-text">{props.top}</h2>
       <h2 className="bottom-text">{props.bottom}</h2>
     </div>
